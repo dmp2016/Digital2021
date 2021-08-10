6cc75efd0205d649cba01401468f327608a67adb
Dmitry <pashutkindv@yandex.ru>
2021-08-11 00:31:44 +0300

    -

--- 1/utils.R
+++ 1/utils.R
@@ -1,18 +1,22 @@
-get_mae <- function(x, y){mean(abs(x - y))}
-
-source("1/prepare_data.R", encoding = "UTF-8")
-
-
-df1 <- df_2019
-df2 <- df_2020
-
-get_df_score(df1, df2, predict_cols){
-  df <- merge(df1, df2, by = c("date", "oktmo"), all.y = T)
+convert_input_date <- function(x){
+  as.Date(paste(substr(x, 7, 10), 
+                "-", 
+                substr(x, 4, 5),
+                "-",
+                substr(x, 1, 2),
+                sep = ""))
   
-  x_predict_cols <- paste0(predict_cols, ".x")
-  y_predict_cols <- paste0(predict_cols, ".y")
 }
 
 
-colnames(df)
-df_tmp <- df %>% filter(is.na(pasta.x))
+convert_date_to_inp <- function(d){
+  return(sapply(d, function(x){
+    s <- as.character(x)
+    return(paste0(
+      substr(s, 9, 10),
+      ".",
+      substr(s, 6, 7),
+      ".",
+      substr(s, 1, 4)))}))
+}
+
