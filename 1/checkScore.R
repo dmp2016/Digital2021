53abebbbde8f2ef09bc98eb47d19fef9efbdd34d
Dmitry <pashutkindv@yandex.ru>
2021-08-11 00:32:09 +0300

    -

---
+++ 1/checkScore.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+
+
+# Датасет с результатами вычислений
+df_test <- df_res %>% 
+  mutate(date = convert_input_date(date))
+
+# Датасет с ответами
+df_answer <- df_train %>% 
+  filter(between(date,
+                 as.Date("2020-04-01"),
+                 as.Date("2020-06-30")))
+
+# Проверим, все ли в порядке?
+if (nrow(df_test) != nrow(df_answer))
+  stop("Размеры датасетов не совпадают")
+
+df_calc <- df_test %>% 
+  inner_join(df_answer, by = c("date", "oktmo"))
+
+# Проверим длину объединенных данных
+if (nrow(df_calc) != nrow(df_answer))
+  stop("Изменился размер датасета после объединения данных")
+
+
+detail_est <- list()
+col_est <- c()
+
+for (col_name in predict_cols){
+  col_test <- paste0(col_name, ".x")
+  col_ans <- paste0(col_name, ".y")
+  # for (cur_oktmo in oktmo_set$oktmo){
+  #   df_calc_reg <- df_calc %>% 
+  #     filter(oktmo == cur_oktmo)
+  #   err <- mean(abs(df_calc_reg[[col_test]] - df_calc_reg[[col_ans]])) /
+  #     mean(df_calc_reg[[col_ans]])
+  #   detail_est[[cur_oktmo]][col_name] <- 1/(1000 * err)
+  # }
+  
+  col_est[col_name] <- mean(abs(df_calc[[col_test]] - df_calc[[col_ans]])) /
+    mean(df_calc[[col_ans]])
+}
+
+score <- 1/(1000 * mean(col_est))
+print(score)
+
+col_est_sort <- sort(col_est)
+
+write_csv(tibble(name = names(col_est_sort), 
+                 est = col_est_sort), 
+          "estimates_col.csv")
+
+# 0.004003584
---
+++ 1/main2020.R
@@ -0,0 +1,433 @@
+library(tidyverse)
+# library(scorer)
+library(Rcpp)
+library(optimization)
+library(lubridate)
+library(randomForest)
+library(car)
+library(RcppRoll)
+# install.packages("optimization")
+# install.packages('Rcpp')
+# install.packages('car')
+# install.packages("randomForest")
+
+
+C_CUR_YEAR <- "2020"
+C_PREV_YEAR <- as.character(as.integer(C_CUR_YEAR) - 1)
+
+
+source("1/prepare_data.r", encoding = "UTF-8")
+
+
+start_date <- as.Date(paste0(C_CUR_YEAR, "-02-01"))
+
+df_train_date <- df_train %>% filter(date >= start_date)
+
+min_date <- min(df_train_date$date)
+
+df_train_date$date_int <- as.integer(difftime(df_train_date$date, 
+                                              min_date, 
+                                              units = "days"))
+
+df_train_date$week <- factor(df_train_date$date_int %% 7)
+
+oktmo_set <- df_train_date %>% select(oktmo) %>% distinct()
+
+df_prev <- df_train %>% 
+  filter(between(date, 
+                 as.Date(paste0(C_PREV_YEAR, "-02-01")) + 1,
+                 as.Date(paste0(C_PREV_YEAR, "-12-31")))) %>% 
+  mutate(date = date + 365 - 1)
+
+
+df_prev_fit <- df_prev %>% 
+  select(date, oktmo, all_of(predict_cols)) %>% 
+  rename(setNames(predict_cols, paste0(predict_cols, ".prev"))) %>% 
+  arrange(date)
+
+
+df_train_date <- merge(df_train_date, df_prev_fit, by = c("oktmo", "date"))
+
+start_predict <- start_date
+finish_predict <- as.Date(paste0(C_CUR_YEAR, "-06-30"))
+
+start_date_int <- as.integer(difftime(start_predict, min_date, units = "days"))
+finish_date_int <- as.integer(difftime(finish_predict, min_date, units = "days"))
+predict_dates_int = start_date_int:finish_date_int
+
+
+cur_oktmo <- "98000000000"
+# 
+col_name <-  "mutton"
+
+res_predict <- NULL
+
+exc_cnt <- 0
+
+except_oktmo <- c()
+except_col <- c()
+estimates <- c()
+
+# prev year only: 0.007461143484185842
+
+shifts <- list()
+
+set.seed(42)
+for (cur_oktmo in oktmo_set$oktmo){
+  print(cur_oktmo)
+  df_train_date_reg <- df_train_date %>% 
+    filter(oktmo == cur_oktmo & date < as.Date(paste0(C_CUR_YEAR, "-04-01")))
+  
+  df_predict_reg <- tibble(oktmo = cur_oktmo,
+                           date_int = predict_dates_int)
+  
+  df_lm <- df_train_date_reg
+  
+  
+  df_prev_fit_reg <- df_prev_fit %>% 
+    filter(oktmo == cur_oktmo) %>% 
+    select(-oktmo) %>% 
+    arrange(date)
+  
+  df_predict <- tibble(date_int = predict_dates_int, 
+                       week = factor(predict_dates_int %% 7),
+                       date = predict_dates_int + min_date)
+  
+  ## Для тестирования качества
+  df_predict <- df_predict %>% 
+    inner_join(df_train %>% 
+                 filter(oktmo == cur_oktmo), 
+               by = c("date")) %>% 
+    rename(setNames(predict_cols, paste0(predict_cols, ".ans")))
+  ## Для тестирования качества
+    
+  df_predict <- merge(df_predict, df_prev_fit_reg,
+                      by = "date")
+  
+  
+  for (col_name in predict_cols){
+    col_name_prev <- paste0(col_name, ".prev")
+    
+    df_lm_part <- df_lm %>% 
+      arrange(date)
+    
+    
+    # col_prev_data <- roll_mean(df_prev_fit_reg[[col_name_prev]],
+    #                            n = 7)
+    
+    col_prev_data <- df_prev_fit_reg[[col_name_prev]]
+    
+    
+    shift_prev <- which.max(sapply(1:25, function(x){
+      cor.test(col_prev_data[x:(x + nrow(df_lm_part) - 1)],
+               df_lm_part[[col_name]])$conf.int[1]}))
+    
+    if (length(shift_prev) == 0)
+      shift_prev <- 1
+    
+    shifts[[cur_oktmo]][col_name] <- shift_prev
+    
+    df_lm_part[["shifted"]] <- col_prev_data[shift_prev:(shift_prev + nrow(df_lm_part) - 1)]
+    df_predict[["shifted"]] <- col_prev_data[shift_prev:(shift_prev + nrow(df_predict) - 1)]
+    
+    # ggplot() +
+    #   geom_point(data = df_predict,
+    #              aes(x = date, 
+    #                  y = .data[[col_name2020]]), 
+    #              col = "red") +
+    #   geom_point(data = df_lm_part,
+    #              aes(x = date, 
+    #                  y = .data[[col_name]]), 
+    #              col = "blue") +
+    #   geom_point(data = df_2020_fit %>% 
+    #                filter(oktmo == cur_oktmo),
+    #              aes(x = date, 
+    #                  y = .data[[col_name2020]]), col = "green")
+    
+    # df_lm_part <- df_lm_part %>%
+    #   arrange(df_lm[[col_name]])
+    # 
+    # if (nrow(df_lm_part[8:(nrow(df_lm_part) - 8), ] %>%
+    #          select(week) %>%
+    #          distinct()) == 7){
+    #   df_lm_part <- df_lm_part[8:(nrow(df_lm_part) - 8), ]
+    # }
+    # else
+    #   df_lm_part <- df_lm_part[7:(nrow(df_lm_part) - 7), ]
+    
+    # tmp_val <- df_lm_part[[col_name]]
+    # 
+    # df_lm_part <- df_lm_part[4:(nrow(df_lm_part) - 3), ]
+    # 
+    # df_lm_part[[col_name]] <- roll_mean(tmp_val,
+    #                                     n = 7)
+    
+    
+    if (nrow(df_lm_part) > 0 & sum(df_lm_part[[col_name]]) != 0)
+    {
+      
+      # lm_formula <- paste(col_name,
+      #                     " ~ date_int + week + ",
+      #                     col_name_prev)
+      
+      # if (length(grep("_value", col_name)) > 0){
+      #   add_col <- " + price"
+      #   price_col <- substr(col_name, 1, nchar(col_name) - 6)
+      # 
+      #   tmp <- df_predict %>% 
+      #     select(date, !!price_col)
+      #   
+      #   tmp$price <- tmp[[price_col]]
+      #   tmp[[price_col]] <- NULL
+      #           
+      #   df_lm_part <- df_lm_part %>% 
+      #     inner_join(tmp, by = "date")
+      # }
+      # else
+      #   add_col <- ""
+      add_col <- ""
+        
+
+      # lm_formula <- paste(col_name,
+      #                     " ~ date_int + week + ",
+      #                     col_name_prev)
+      # 
+      # fit.glm <- glm(as.formula(lm_formula),
+      #                data = df_lm_part)
+      # 
+      # sf <-  summary(fit.glm)
+      # 
+      # if (last(sf$coefficients[, 1]) < 0){
+      #   lm_formula <- paste(col_name,
+      #                       " ~ date_int + week",
+      #                       add_col)
+      #   
+      #   fit.glm <- glm(as.formula(lm_formula),
+      #                  data = df_lm_part)
+      # }
+      # 
+      # 
+      # df_predict <- df_predict %>% mutate(!!col_name := predict(fit.glm, df_predict))
+      
+      data <- ts(df_lm_part[[col_name]], frequency = 7)
+      L <- BoxCox.lambda(data, method="loglik")
+      
+      fit.arima <- auto.arima(data, lambda=L)
+      
+      # h = difftime(as.Date("2021-06-30"),
+      #              as.Date("2021-04-01"),
+      #              units = "days")
+      fcast.arima <- forecast(fit.arima, 3, lambda=L)
+      df_predict[[col_name]] <- fcast.arima$mean
+
+      
+      if (min(df_lm_part[[col_name]]) > 0){
+        rate <- mean(df_predict[[col_name]][between(df_predict$date, 
+                                                    as.Date(paste0(C_CUR_YEAR, "-06-20")), 
+                                                    as.Date(paste0(C_CUR_YEAR, "-06-30")))]) /
+          mean(df_predict[[col_name]][between(df_predict$date, 
+                                              as.Date(paste0(C_CUR_YEAR, "-04-01")), 
+                                              as.Date(paste0(C_CUR_YEAR, "-04-10")))])
+        if (rate < 1/2 | rate > 2){
+          exc_cnt <- exc_cnt + 1
+          print(paste("except", 
+                      cur_oktmo, 
+                      col_name))
+          except_oktmo[exc_cnt] <- cur_oktmo
+          except_col[exc_cnt] <- col_name
+          
+
+          if (col_name != "сucumbers_tomatoes"){
+            fit.exc <- glm(as.formula(paste(col_name,
+                                            " ~ date_int + week")),
+                           data = df_lm_part)
+
+            df_predict <- df_predict %>%
+              mutate(!!col_name := predict(fit.exc,
+                                           df_predict))
+
+
+            c_val <- mean(df_predict[[col_name]][between(df_predict$date,
+                                                         as.Date(paste0(C_CUR_YEAR, "-04-15")),
+                                                         as.Date(paste0(C_CUR_YEAR, "-04-20")))])
+            df_predict[[col_name]][df_predict$date > as.Date("2021-04-15")] <- c_val
+            #              df_predict[[col_name]][df_predict$date == as.Date("2021-04-15")]
+
+          }
+          else{
+            fit.exc <- glm(as.formula(paste(col_name,
+                                            " ~ shifted")),
+                           data = df_lm_part)
+
+            df_predict <- df_predict %>%
+              mutate(!!col_name := predict(fit.exc,
+                                           df_predict))
+
+          }
+        }
+      }
+      df_predict[[col_name]] <- ifelse(df_predict[[col_name]] < 0, 
+                                       0, 
+                                       df_predict[[col_name]])
+    }
+    else
+    {
+      print(0)
+      df_predict <- df_predict %>% 
+        mutate(!!col_name := 0)
+    }
+    
+    df_1 <- df_train %>% 
+      filter(between(date,
+                     as.Date("2020-04-01"),
+                     as.Date("2020-06-30")))
+    
+    err <- mean(abs(df_predict[[paste0(col_name, ".ans")]] -  df_predict[[col_name]])) /
+      mean(abs(df_predict[[paste0(col_name, ".ans")]]))
+    
+    estimates[paste0(cur_oktmo, 
+                     ".",
+                     col_name)] <- 1 / (1000 * err)
+    df_predict_reg <- cbind(df_predict_reg, 
+                            df_predict %>% 
+                              select(!!col_name))
+  }
+  if (is.null(res_predict))
+    res_predict <- df_predict_reg
+  else
+    res_predict <- rbind(
+      res_predict,
+      df_predict_reg)
+}
+
+est_sort <- sort(estimates)
+
+write_csv(tibble(name = names(est_sort), est = est_sort), "estimates.csv")
+
+
+print(exc_cnt)
+res_predict$date <- res_predict$date_int + min_date
+
+df_test <- read_csv("1/test.csv", 
+                    col_types = cols(.default = col_character()))
+
+df_test$date_format <- convert_input_date(df_test$date) - 365
+df_test$date <- convert_date_to_inp(df_test$date_format)
+
+df_res <- merge(df_test %>% 
+                  select(region, okato, oktmo, date, date_format), 
+                res_predict %>% 
+                  filter(date >= as.Date(paste0(C_CUR_YEAR, "-04-01"))),
+                by.x = c("oktmo", "date_format"),
+                by.y = c("oktmo", "date"),
+                sort = F) %>% 
+  select(all_of(colnames(df_test))) %>% 
+  select(-date_format)
+
+write_csv(df_res, "1/mytest_2020.csv")
+
+write_csv(res_predict, "1/all_predict.csv")
+
+
+# Проверка корректности порядка строк и столбцов в результирующем наборе
+# sum(df_tmp$date != df_test$date | 
+#       df_tmp$oktmo != df_test$oktmo)
+# 
+# 
+# sum(colnames(df_tmp) != colnames(df_test))
+# sum(colnames(df_test %>% select(-date_format))!= colnames(df_res))
+
+
+
+######################################################
+# Test prediction on graphic
+######################################################
+cur_oktmo <- "71000000000"
+cur_oktmo <- "26000000000"
+cur_oktmo <- "64000000000"
+cur_oktmo <- "75000000000"
+cur_oktmo <- "33000000000"
+col_name <- "herring_value"
+col_name <- "rice"
+col_name <- "сucumbers_tomatoes"
+ggplot(data = df_train_date %>% 
+         filter(oktmo == cur_oktmo)) +
+  geom_point(aes(x = date, 
+                 y = .data[[col_name]]), 
+             col = "blue") +
+  geom_line(aes(x = date, 
+                y = .data[[col_name]]), 
+            col = "blue") +
+  geom_smooth(aes(x = date, 
+                  y = .data[[col_name]]), 
+              method = "lm") +
+  geom_point(data = res_predict %>% 
+               filter(oktmo == cur_oktmo), 
+             aes(x = date, 
+                 y = .data[[col_name]]), 
+             col = "red") +
+  geom_point(data = df_prev_fit %>% 
+               filter(oktmo == cur_oktmo),
+             aes(x = date, 
+                 y = .data[[paste0(col_name,
+                                   ".prev")]]), col = "green")
+
+
+
+for (ind in 1:exc_cnt){
+  col_name <- except_col[ind]
+  cur_oktmo <- except_oktmo[ind]
+  show(ggplot(data = df_train_date %>% 
+                filter(oktmo == cur_oktmo)) +
+         geom_point(aes(x = date, 
+                        y = .data[[col_name]]), 
+                    col = "blue") +
+         geom_line(aes(x = date, 
+                       y = .data[[col_name]]), 
+                   col = "blue") +
+         geom_smooth(aes(x = date, 
+                         y = .data[[col_name]]), 
+                     method = "lm") +
+         geom_point(data = res_predict %>% 
+                      filter(oktmo == cur_oktmo), 
+                    aes(x = date, 
+                        y = .data[[col_name]]), 
+                    col = "red") +
+         geom_point(data = df_prev_fit %>% 
+                      filter(oktmo == cur_oktmo),
+                    aes(x = date, 
+                        y = .data[[paste0(col_name,
+                                          ".prev")]]), col = "green") +
+         ggtitle(cur_oktmo))
+  
+}
+
+
+col_name <- "mutton"
+
+for (cur_oktmo in oktmo_set$oktmo){
+  show(ggplot(data = df_train_date %>% 
+                filter(oktmo == cur_oktmo)) +
+         geom_point(aes(x = date, 
+                        y = .data[[col_name]]), 
+                    col = "blue") +
+         geom_line(aes(x = date, 
+                       y = .data[[col_name]]), 
+                   col = "blue") +
+         geom_smooth(aes(x = date, 
+                         y = .data[[col_name]]), 
+                     method = "lm") +
+         geom_point(data = res_predict %>% 
+                      filter(oktmo == cur_oktmo), 
+                    aes(x = date, 
+                        y = .data[[col_name]]), 
+                    col = "red") +
+         geom_point(data = df_prev_fit %>% 
+                      filter(oktmo == cur_oktmo),
+                    aes(x = date, 
+                        y = .data[[paste0(col_name,
+                                          ".prev")]]), col = "green") +
+         ggtitle(cur_oktmo))
+  
+}
+
